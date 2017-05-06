893fad8daa77fb583874c0973d8513a5a4f398d5
Stay.Blossom <Stay.Blossom@StayBlossomui-MacBook-Air.local>
2017-05-06 20:29:52 +0900

    Added Post

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog', 
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
